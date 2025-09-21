fa058265bd06897988bb035a2898ca2d6bed1eb1
BalamuraliSiripurapu <sspbalamurali@gmail.com>
2025-09-21 15:55:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [],
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Database initialization
